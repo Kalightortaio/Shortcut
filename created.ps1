5fe9ac2221204bd961e585305b21b228005fd8d3
Krishna Kokatay <kokataykrishna@gmail.com>
2019-01-24 15:36:01 -0800

    Shortcut v1.1

--- created.ps1
+++ created.ps1
@@ -1,6 +1,6 @@
 . (Join-Path $PSScriptRoot endofscript.ps1)
 
-function create($create, $optional, $optional2='~\') {
+function create($create, $optional, $optional2='$env:USERPROFILE') {
     If ($create -eq 'shortcut') {
         Write-Host "You should never ever be able to see this message, ever, at all. Please report to dev. Quantum Mechanics has failed us."
 #Replace
