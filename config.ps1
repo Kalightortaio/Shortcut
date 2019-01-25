87c896fcbe9e3ca71da253052e816de1521a1cbf
Krishna Kokatay <kokataykrishna@gmail.com>
2019-01-24 16:51:09 -0800

    Initial Commit

--- config.ps1
+++ config.ps1
@@ -1,8 +1,8 @@
 #Configuration File for the shortcut module
 
 #Change letter to the default drive, for example I had multiple hard drives so I used D.
-#This will be used for sd documents, sd downloads, and sd desktop.
+#This will be used for hardcoded access to your documents, downloads, and desktop.
 $DefaultDrive = "C"
 
-#Change letter to the Windows boot drive, for example I used C. This will be used for the installation process.
+#Change letter to the Windows boot drive, for example I used C. This will be used for the installation process and other misc. functions.
 $BootDrive = "C"
