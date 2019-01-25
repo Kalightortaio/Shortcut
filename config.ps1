67c72aba540a77f17ab7ad3c804e0c6cff58fc29
Krishna Kokatay <kokataykrishna@gmail.com>
2019-01-24 16:58:13 -0800

    Initial commit

--- config.ps1
+++ config.ps1
@@ -3,6 +3,6 @@
 #Change letter to the default drive, for example I had multiple hard drives so I used D.
 #This will be used for hardcoded access to your documents, downloads, and desktop.
 $DefaultDrive = "C"
-
+ 
 #Change letter to the Windows boot drive, for example I used C. This will be used for the installation process and other misc. functions.
 $BootDrive = "C"
