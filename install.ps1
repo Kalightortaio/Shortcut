a31670b947f3a293215c52489a9282ff17267943
Krishna Kokatay <kokataykrishna@gmail.com>
2019-01-26 16:32:48 -0800

    Another bugfix, another day
    Fixed an ownership issue, preventing the script and the user from editing files.

--- install.ps1
+++ install.ps1
@@ -24,6 +24,7 @@ Move-Item -Force -Path "$folderpath\packager_backup.txt" -Destination "$($BootDr
 Move-Item -Force -Path "$folderpath\LICENSE" -Destination "$($BootDrive):\Program Files\WindowsPowerShell\Modules\Shortcut"
 Move-Item -Force -Path "$folderpath\README.md" -Destination "$($BootDrive):\Program Files\WindowsPowerShell\Modules\Shortcut"
 Move-Item -Force -Path "$folderpath\endofscript.ps1" -Destination "$($BootDrive):\Program Files\WindowsPowerShell\Modules\Shortcut"
+takeown /f "$($BootDrive):\Program Files\WindowsPowerShell\Modules\Shortcut" /r
 $wshell = New-Object -ComObject Wscript.Shell
 $wshell.Popup("Operation Completed!",0,"Shortcut Installer",0x1)
 exit
