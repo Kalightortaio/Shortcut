82028d38845796227b0b76f81dc25058b824ebf2
Krishna Kokatay <kokataykrishna@gmail.com>
2019-02-01 20:48:25 -0800

    Shortcut v1.3

--- endofscript.ps1
+++ endofscript.ps1
@@ -1,16 +1,16 @@
 #This file was created to prevent any future edits to the created.ps1 file. If error checking or extraneous commands need to be added at the end of the script, it is now permissible without interferring with user data.
 
 function endofscript($endofscript, $optional, $optional2='$env:USERPROFILE') {
-      if ($err) {
-          Write-Host " "
-          Write-Host "Shortcut cannot be deleted, it does not exist. Use 'shortcut created' to view your current list."
-          Write-Host " "
-          $err = 0
-          break
-      }
       Write-Host " "
       Write-Host "Missing Destination! You probably need to program it in..."
       Write-Host " "
       Write-Host "Use shortcut help for documentation."
       Write-Host " "
+      $global:sddebugcode=0;return 0 | Out-Null;
 }
+
+#Debug Codes
+#0 - End of Script / Function was not found
+#1 - Error was returned when function was executed
+#2 - Function is hardcoded
+#3 - Function is softcoded
