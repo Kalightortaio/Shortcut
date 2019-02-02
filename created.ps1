26b84dd1de4fef19a6f14e070a655b7102a7d1e0
Krishna Kokatay <kokataykrishna@gmail.com>
2019-02-01 21:03:27 -0800

    Removed an unnecessarily long error message.
    With the new debug codes, all of that is obsolete!

--- created.ps1
+++ created.ps1
@@ -1,6 +1,8 @@
 . (Join-Path $PSScriptRoot endofscript.ps1)
 
 function create($create, $optional, $optional2='$env:USERPROFILE') {
+    If (1 -eq 0) {
+        $global:sddebugcode=1;return 0 | Out-Null;
 #Replace
     }
     endofscript $create, $optional, $optional2
