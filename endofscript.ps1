808a85263e3b91748a744277001fc5b13ac60116
Krishna Kokatay <kokataykrishna@gmail.com>
2019-02-01 21:19:47 -0800

    Added the debug code reference here
    I might move them in the future to a better file

--- endofscript.ps1
+++ endofscript.ps1
@@ -1,11 +1,11 @@
 #This file was created to prevent any future edits to the created.ps1 file. If error checking or extraneous commands need to be added at the end of the script, it is now permissible without interferring with user data.
 
 function endofscript($endofscript, $optional, $optional2='$env:USERPROFILE') {
-      Write-Host " "
-      Write-Host "Missing Destination! You probably need to program it in..."
-      Write-Host " "
-      Write-Host "Use shortcut help for documentation."
-      Write-Host " "
+      Write-Output " "
+      Write-Output "Missing Destination! You probably need to program it in..."
+      Write-Output " "
+      Write-Output "Use shortcut help for documentation."
+      Write-Output " "
       $global:sddebugcode=0;return 0 | Out-Null;
 }
 
