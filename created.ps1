030895624724a66fee59980aa7a82158f8f00f36
Krishna Kokatay <kokataykrishna@gmail.com>
2019-02-01 20:49:36 -0800

    Shortcut v1.3

--- created.ps1
+++ created.ps1
@@ -3,9 +3,8 @@
 function create($create, $optional, $optional2='$env:USERPROFILE') {
     If ($create -eq 'shortcut') {
         Write-Host "You should never ever be able to see this message, ever, at all. Please report to dev. Quantum Mechanics has failed us."
+        $global:sddebugcode=1;return 0 | Out-Null;
 #Replace
     }
     endofscript $create, $optional, $optional2
 }
-
-
