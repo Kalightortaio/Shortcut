d3945f1856f57c9e145fdcf78e48d11825997331
Krishna Kokatay <kokataykrishna@gmail.com>
2019-05-01 09:28:26 -0700

    1.3.1

--- config.ps1
+++ config.ps1
@@ -1,11 +1,4 @@
 #Configuration File for the shortcut module
 
-#Change the letter to the default drive, for example I had multiple hard drives so I used "D".
-#This will be used for hardcoded access to your documents, downloads, and desktop.
-$DefaultDrive = "C"
- 
-#Change the letter to the Windows boot drive, for example I used "C". This will be used for the installation process and other misc. functions.
-$BootDrive = "C"
-
 #Change the number from zero to literally anything else to successfully install the shortcut module.
 $ValueMustNotBeZero = 0
