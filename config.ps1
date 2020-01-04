10264e09e2a34ad84040d1f3200c570e1e3b4992
Krishna Kokatay <kokataykrishna@gmail.com>
2020-01-03 21:52:30 -0800

    1.5

--- config.ps1
+++ config.ps1
@@ -2,3 +2,6 @@
 
 #Change the number from zero to literally anything else to successfully install the shortcut module.
 $ValueMustNotBeZero = 0
+
+#Change the drive letter to match what corresponds with the Windows Boot Drive. Default is set to "C"
+$BootDrive = "C"
