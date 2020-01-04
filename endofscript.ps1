4b4831ca77bc2eb72aa2d112cc5b64769b067f8b
Krishna Kokatay <kokataykrishna@gmail.com>
2020-01-03 22:31:01 -0800

    1.5

--- endofscript.ps1
+++ endofscript.ps1
@@ -1,12 +1,11 @@
 #This file was created to prevent any future edits to the created.ps1 file. If error checking or extraneous commands need to be added at the end of the script, it is now permissible without interferring with user data.
 
 function endofscript($endofscript, $optional, $optional2='$env:USERPROFILE') {
-    try {
-        Set-Location -Path $endofscript[0]
-    }
-    catch {
+    Set-Location -Path $endofscript[0] -ErrorAction:SilentlyContinue
+    If (!($?)) {
         Write-Host " "
         Write-Host "A destination at the specified path does not exist. Use sd help for a list of appropriate actions."
+        Write-Host " "
     }
     $global:sddebugcode=0;return 0 | Out-Null;
 }
